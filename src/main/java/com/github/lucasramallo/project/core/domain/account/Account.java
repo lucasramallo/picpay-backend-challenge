627eeb1d82978f412f093740f4d1f86199520d34
lucasramallo <lucasdebrito.jd@gmail.com>
2024-04-22 14:47:02 -0300

    feat: creating account domain

--- src/main/java/com/github/lucasramallo/project/core/domain/account/Account.java
+++ src/main/java/com/github/lucasramallo/project/core/domain/account/Account.java
@@ -1,4 +1,16 @@
 package com.github.lucasramallo.project.core.domain.account;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Account {
+    private UUID id;
+    private BigDecimal balance;
 }
